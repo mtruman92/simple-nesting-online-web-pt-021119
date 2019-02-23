0a0d9ef48bdf60263642039f7f757c71e9ad4be4
Mickae Truman <mtruman@nyit.edu>
2019-02-23 00:08:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[:grace_hopper][languages.to_s]
+     programmer_hash[:grace_hopper][:languages.to_s]
 end
 
 def adding_matz
